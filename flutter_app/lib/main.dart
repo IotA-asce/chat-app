bae5d82e21c2ce8649b40214478710a5882e99fd
IotA-asce <muinnasif@gmail.com>
2022-07-23 21:43:01 +0530

    test integrity

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/random_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Flutter chat app"),
         ),
-        body: const Text("this line"),
+        body: Center(
+          child: RandomWords(),
+        ),
       ),
     );
   }
